d6f00b653e1c19c3560b06d91ab763a63bd60b82
jebcoding <johnbvillegas11@gmail.com>
2024-10-12 18:53:36 +0800

    commit12-10

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
 
+const title = {
+    img: '/tm logo.png',
+
+};
+
 function App() {
   return (
-    <div>
-      <h1>TM Worship</h1>
+    <div className='container'>
+       <img 
+        src={title.img}  
+        className='w-[250px] h-[250] relative bottom-10'
+        id='tmlogo'
+    />  
+      <h1 className='' id='worship'>WORSHIP</h1>
+   
     </div>
-  );
+    );
 }
 
 export default App;
