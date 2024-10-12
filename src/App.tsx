b6e0df39be25b48e00b908c477fc899694f401e8
jebcoding <johnbvillegas11@gmail.com>
2024-10-12 19:05:27 +0800

    12-10

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 
 const title = {
     img: '/tm logo.png',
+    img2: '/music icon.png'
 
 };
 
@@ -12,9 +13,13 @@ function App() {
         src={title.img}  
         className='w-[250px] h-[250] relative bottom-10'
         id='tmlogo'
-    />  
+       />  
       <h1 className='' id='worship'>WORSHIP</h1>
-   
+      <img
+        src= {title.img2}
+        className='w-[100px] h-[150px] relative left-[8vh]'
+        id='music_icon'
+      />
     </div>
     );
 }
