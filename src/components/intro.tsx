f8ea15b4cdb3d3881e950ea04be072f75b7e82ad
jebcoding <johnbvillegas11@gmail.com>
2024-10-12 21:00:08 +0800

    12-10

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -9,14 +9,14 @@ function Intro() {
 
   useEffect(() => {
     AOS.init({ duration: 1000 });
-    const timer = setTimeout(() => navigate('/home'), 3000);
+    const timer = setTimeout(() => navigate('/home'), 10000);
     return () => clearTimeout(timer);
   }, [navigate]);
 
   return (
     <div data-aos="flip-up" className="container">
       <img src="/tm logo.png" alt="TM Logo" id='tmlogo' className="w-[250px] h-[250px] relative bottom-10" />
-      <h1 className="text-center" id="worship">worship</h1>
+      <h1 className="text-center" id="worship">WORSHIP</h1>
       <img src="/music icon.png" id='music_icon' alt="Music Icon" className="w-[100px] h-[150px] relative left-[8vh]" />
     </div>
   );
