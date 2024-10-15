7a5daf346b21ba1d23c65c25b1e1263bd994d20a
jebcoding <johnbvillegas11@gmail.com>
2024-10-15 17:09:28 +0800

    15-10

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,7 +3,7 @@ import './css/content.css'
 
 function main() {
   return (
-    <div className='main  relative rounded-[10px] left-[20px] top-16 '>
+    <div className='main  relative rounded-[10px] left-[20px] top-10 '>
          
     </div>
   )
